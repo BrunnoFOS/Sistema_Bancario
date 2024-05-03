a0f551d50d6ceead4b83a4979fa79050857edfaa
BrunnoFOS <brunnofos@outlook.com>
2024-05-02 22:30:43 -0300

    Tabela Clientes

--- schema.sql
+++ schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE contas (
     atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255),
+    cpf VARCHAR(14) UNIQUE,
+    data_nascimento DATE,
+    endereco VARCHAR(255)
+);
